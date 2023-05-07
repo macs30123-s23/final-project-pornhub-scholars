feb3d680ae022cd6d21d10c285e6b1835a9f3f3d
lbitsiko <l.bitsikokos@gmail.com>
2023-05-07 14:45:12 -0500

    update

--- scraper.py
+++ scraper.py
@@ -18,6 +18,17 @@ def create_database_table():
                         video_url TEXT,
                         comment_text TEXT,
                         upvotes INTEGER)''')
+    
+    cursor.execute("""CREATE TABLE IF NOT EXISTS video_info (
+                        title TEXT,
+                        creator_name TEXT,
+                        creator_href TEXT,
+                        video_url TEXT,
+                        views TEXT, 
+                        rating TEXT, 
+                        year_added TEXT,
+                        categories TEXT
+                        )""")
     conn.commit()
     conn.close()
 
@@ -34,36 +45,65 @@ def insert_comment(username, username_href, video_url, comment_text, upvotes):
     conn.commit()
     conn.close()
 
+def insert_video(title, creator_name, creator_href, url, views, rating, year_added, categories):
+    conn = sqlite3.connect('comments.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        """INSERT INTO video_info 
+           (title, creator_name, creator_href, video_url, views, rating, year_added, categories)
+           VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """, 
+        (title, creator_name, creator_href, url,views, rating, year_added, categories)
+        )
+    conn.commit()
+    conn.close()
+
+def insert_creator():
+    pass
+
+def scrape_and_insert_comments(porn_soup, video_url):
+    user_data_blocks_list = porn_soup.findAll("div", {"class": "topCommentBlock clearfix"})
+    comment_data_list= porn_soup.findAll("div", {"class": "commentMessage"})
+    upvotes_data_list = porn_soup.findAll("div", {"class":"actionButtonsBlock"})
+    # #Getting comments
+    for user_block, comment_block, upvote_block \
+        in zip(user_data_blocks_list[:-1],
+            comment_data_list[:-1],
+            upvotes_data_list[:-1]):
+        if user_block.find("a"):
+            user_href = user_block.find("a").get('href')
+            user_name = user_block.find("a").find("img").get('title')
+            comment_text = comment_block.span.text
+            upvote = upvote_block.find("span").text
+            insert_comment(user_name, user_href, video_url, comment_text, upvote)
+
+def scrape_and_insert_video_info(porn_soup, video_url):
+    video_title = porn_soup.find("span", {"class":"inlineFree"}).text
+    video_views = porn_soup.find('li', {"class":"views"}).span.text
+    video_rating_up = porn_soup.find('li', {"class":"rating up"}).span.text 
+    video_added = porn_soup.find('li', {"class":"added"}).text
+    video_categories = [i.text for i in porn_soup.findAll('span', {'class':'crowdTitle'})]
+
+    # TODO: create profile table
+    creator_video_count = porn_soup.find("div", {"class":"userInfoContainer"}).find("span",{"class":"videosCount"}).text
+    subscribers_count = porn_soup.find("div", {"class":"userInfoContainer"}).find("span",{"class":"subscribersCount"}).text
+
+    if porn_soup.find("a", {"class":"gtm-event-link bolded"}):
+        creator_name =  porn_soup.find("a", {"class":"gtm-event-link bolded"}).text
+        creator_href =  porn_soup.find("a", {"class":"gtm-event-link bolded"})['href']
+    else:
+        creator_name = porn_soup.find("div", {"class":"userInfoContainer"}).find("a").text
+        creator_href = porn_soup.find("div", {"class":"userInfoContainer"}).find("a").get('href')
+    insert_video(video_title, creator_name, creator_href,video_url, video_views, video_rating_up, video_added, str(video_categories))
+
 if __name__ == "__main__":
     create_database_table()
-    N = 5
+    N = 10
     for i in range(N):
         response = requests.get(starting_url, headers=headers)
         video_url = response.url
         print(video_url)
         porn_soup = BeautifulSoup(response.text, "html.parser")
 
-        user_data_blocks_list = porn_soup.findAll("div", {"class": "topCommentBlock clearfix"})
-        comment_data_list= porn_soup.findAll("div", {"class": "commentMessage"})
-        upvotes_data_list = porn_soup.findAll("div", {"class":"actionButtonsBlock"})
-        # #Getting comments
-        for user_block, comment_block, upvote_block \
-            in zip(user_data_blocks_list[:-1],
-                comment_data_list[:-1],
-                upvotes_data_list[:-1]):
-            if user_block.find("a"):
-                user_href = user_block.find("a").get('href')
-                user_name = user_block.find("a").find("img")['title']
-                comment_text = comment_block.span.text
-                upvote = upvote_block.find("span").text
-                print((user_name, user_href, video_url, comment_text, upvote))
-                insert_comment(user_name, user_href, video_url, comment_text, upvote)
-
-    #     porn_comment_data = comment_block.findAll("span")
-    #     if porn_comment_data:
-    #         username_href = porn_comment_data[0].find('a')['href']
-    #         username = porn_comment_data[0].find('a').text
-
-    #         comment_text = porn_comment_data[1].text
-    #         upvotes = porn_comment_data[2].text
-    #         # insert_comment(username, username_href, video_url, comment_text, upvotes)
+        scrape_and_insert_video_info(porn_soup, video_url)
+        scrape_and_insert_comments(porn_soup, video_url)
